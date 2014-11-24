b1cffd4e81332509a4caeb31309ea8d2dff858f1
Morgan Plant <morgantheplant@gmail.com>
2014-11-24 15:50:46 -0800

    Trying to add demos to browserify seed

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ server.route({
     method: 'GET',
     path: '/ball',
     handler: function (request, reply) {
-        reply.file('./ball demo/ballbounce.html');
+        reply.file('./balldemo/ballbounce.html');
     }
 });
 
