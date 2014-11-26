d5f0f385d448d5e332447cf86f547040ab5ca010
Morgan Plant <morgantheplant@gmail.com>
2014-11-26 10:58:39 -0800

    (feat) added fancy demo pages

--- public/growdemo/containerGrow.js
+++ public/growdemo/containerGrow.js
@@ -36,11 +36,13 @@ define(function(require, exports, module) {
 
     var tvScreen = new Surface({
         size:[116,87],
-        content: '<iframe src="./growdemo/scroll.html" style="height:87px; width:116px"></iframe>',
+        content: '<div id="wrapper"><iframe src="./growdemo/scroll.html" scrolling="no" seamless="seamless" frameBorder="0" style="height:87px; width:116px"></iframe></div>',
         properties: {
         zIndex: '10'
       }
     });
+    
+   
 
     var rotater = new StateModifier({
         transform: Transform.rotateZ(Math.PI/16),
@@ -99,9 +101,8 @@ define(function(require, exports, module) {
 
   
   //start expading 100px from bottom of page
-  console.log(window.innerHeight)
   var padding = window.innerHeight - 100;
-  var maxHeight = window.innerHeight + padding - 300; 
+  var maxHeight = 400;//window.innerHeight + padding - 400; 
   var containerHeight = parseFloat(document.getElementById('famous-container').style.height);
   var getContainerHeight = function(){ return parseFloat(document.getElementById('famous-container').style.height); }
   var distanceTravelled = function(){ return window.pageYOffset + padding - targetPosition }
@@ -111,7 +112,7 @@ define(function(require, exports, module) {
     
     if(getContainerHeight() > 10 && !transCalled) {
       aligner.halt();
-      aligner.setTransform(Transform.translate(150,75,0), spring);
+      aligner.setTransform(Transform.translate(110,25,0), spring);
       transCalled = true;
        transOutCalled = false;
     }
